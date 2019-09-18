90e3f0bd214ca0381dc95f1a826167ba3635975e
rleofield <richard@rleofield.de>
2019-09-18 23:35:39 +0200

    add

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    string a = "Hello";
+    string b = "World";
+    string c = a + " " + b;
+    cout << c << endl;
+
+    cout << std::endl;
+    return 0;
+}
